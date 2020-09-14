b4117b179a732a1e1320def15f971dc749afd2f0
Diego Andrés Ipuz García <dandresipuz@gmail.com>
2020-09-13 22:31:33 -0500

    firts

--- proyect/src/app/app.component.ts
+++ proyect/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyect';
+  activo:string;
+  show:boolean = true;
+  inicio:string;
+  // languages:string[] = ['HTML', 'ANGULAR','PHP'];
+  persons:Array<any> = [
+    {lastname:'Castañeda',name:'Andrea',age:34},
+    {lastname:'Olarte',name:'Brayan',age:18},
+    {lastname:'Ipuz',name:'Samantha',age:7},
+    {lastname:'Ipuz',name:'Diego',age:34},
+  ]
+  items:number[] = [1&15];
 }
